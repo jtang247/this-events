a20dc1e3503e5f9b09c048458cd873e7fa0fd3ad
jkt247 <Jonathon@sos-hawaii.com>
2017-01-25 22:07:31 -1000

    pau

--- js/app.js
+++ js/app.js
@@ -2,3 +2,22 @@
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
 
 
+var restaurant = document.getElementsByClassName("name");
+
+for(i=0;i<restaurant.length;i++){
+	restaurant[i].addEventListener("click", showMenu);
+}
+
+function showMenu(){
+	var getMenu=this.querySelectorAll(".menu")[0];
+    if(getMenu.style.display === "none"){
+    	getMenu.style.display = "block";
+    }else{
+    	getMenu.style.display = "none";
+    }
+}
+
+// target the class and get the element
+// see that presents an array
+//array means forloops
+//
